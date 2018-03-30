5caa4a0aa442385bb7529fd8e26934982863c4db
nikymorg <nicole.morgan@gmail.com>
2018-03-30 15:07:12 -0400

    add option name

--- db/migrate/20180330182536_create_elections.rb
+++ db/migrate/20180330182536_create_elections.rb
@@ -1,5 +1,7 @@
 class CreateElections < ActiveRecord::Migration[5.1]
   def change
-    
+    create_table :elections do |t|
+      t.string :status
+    end
   end
 end
--- db/migrate/20180330182743_create_options.rb
+++ db/migrate/20180330182743_create_options.rb
@@ -2,6 +2,7 @@ class CreateOptions < ActiveRecord::Migration[5.1]
   def change
     create_table :options do |t|
       t.integer :election_id
+      t.string :name
       t.string :status
     end
   end
