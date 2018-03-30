b84bdc7041e32ff4fc6fa36e775a7e07a8e488b6
nikymorg <nicole.morgan@gmail.com>
2018-03-30 15:05:59 -0400

    add vote election validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,13 @@ class Vote < ActiveRecord::Base
   belongs_to :election
   belongs_to :option
   belongs_to :voter
-  
+
+  validate :election_id
+
+  def election_id
+    if self.election_id != self.option.election_id
+      errors.add(:election_id, 'election id must match option.election_id')
+    end
+  end
+
 end
