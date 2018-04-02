af12762b6905b4c5f2afe185829c0853ee562ca6
Rob Cobb <rwcobbjr@gmail.com>
2018-04-02 09:49:13 -0400

    remove election status -- unused

--- db/migrate/20180330182536_create_elections.rb
+++ db/migrate/20180330182536_create_elections.rb
@@ -3,7 +3,6 @@ class CreateElections < ActiveRecord::Migration[5.1]
     create_table :elections do |t|
       t.string :name, null: false
       t.text :description
-      t.integer :status, null: false, default: 0
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20180330182752) do
   create_table "elections", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
-    t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
